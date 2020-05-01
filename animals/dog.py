ac53dfd6bf742da10e42dd251d4dd82c3b3fbeb9
gorandp <gorandp@outlook.com>
2020-05-01 19:39:41 -0300

    comment

--- animals/dog.py
+++ animals/dog.py
@@ -4,5 +4,7 @@ class Dog(Animal):
     def __init__(self):
         pass
 
+# Not recomended
+# For this it is important that in sounds folder, __init__.py defines __all__ list with the modules to import
 from sounds import *
 beep.d()
